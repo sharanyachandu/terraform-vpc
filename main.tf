042137724742856921accfae5c037c6959528d44
sharanya chandu <you@example.com>
2023-06-18 00:46:53 +0530

    auto save

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
   source     = "./vendor/modules/vpc"
   VPC_CIDR   = var.VPC_CIDR
   ENV        = var.ENV
+  PUBLIC_SUBNET_CIDR       = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR      = var.PRIVATE_SUBNET_CIDR 
+  AZ                       = var.AZ
 }
