c1cf1b59196656bf47e2deddea6cac34f1e21bfd
sharanya chandu <you@example.com>
2023-06-19 12:08:53 +0530

    auto save

--- main.tf
+++ main.tf
@@ -7,4 +7,6 @@ module "vpc" {
   AZ                       = var.AZ
   DEFAULT_VPC_ID           = var.DEFAULT_VPC_ID
   DEFAULT_VPC_CIDR         = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT           = var.DEFAULT_VPC_RT
+
 }
