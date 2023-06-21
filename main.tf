b13598917b6bfbb980ef74da8596532c6f195a38
sharanya chandu <you@example.com>
2023-06-21 09:28:55 +0530

    auto save

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
-  source     = "./vendor/modules/vpc"
-  VPC_CIDR   = var.VPC_CIDR
-  ENV        = var.ENV
-  PUBLIC_SUBNET_CIDR       = var.PUBLIC_SUBNET_CIDR
-  PRIVATE_SUBNET_CIDR      = var.PRIVATE_SUBNET_CIDR 
-  AZ                       = var.AZ
-  DEFAULT_VPC_ID           = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR         = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT           = var.DEFAULT_VPC_RT
+  source               = "./vendor/modules/vpc"
+  VPC_CIDR             = var.VPC_CIDR
+  ENV                  = var.ENV
+  PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR 
+  AZ                   = var.AZ
+  DEFAULT_VPC_ID       = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR     = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT       = var.DEFAULT_VPC_RT
 
 }
